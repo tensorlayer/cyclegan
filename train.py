77ac14c1f8ecde403f1828eb360b6e7f044bb425
Luo Mai <luo.mai.cs@gmail.com>
2019-11-09 04:37:38 +0000

    Simplify the cyclegan code.

--- train.py
+++ train.py
@@ -19,103 +19,7 @@ sample_B = im_test_B[0:25]
 tl.vis.save_images(sample_A, [5, 5], flags.sample_dir+'/_sample_A.png')
 tl.vis.save_images(sample_B, [5, 5], flags.sample_dir+'/_sample_B.png')
 
-def train():
-    Gab = models.get_G(name='Gab')
-    Gba = models.get_G(name='Gba')
-    Da  = models.get_D(name='Da')
-    Db  = models.get_D(name='Db')
-
-    Gab.train()
-    Gba.train()
-    Da.train()
-    Db.train()
-
-    lr_v = tf.Variable(flags.lr_init)
-    optimizer_Gab = tf.optimizers.Adam(lr_v, beta_1=flags.beta_1)
-    optimizer_Gba = tf.optimizers.Adam(lr_v, beta_1=flags.beta_1)
-    optimizer_Da = tf.optimizers.Adam(lr_v, beta_1=flags.beta_1)
-    optimizer_Db = tf.optimizers.Adam(lr_v, beta_1=flags.beta_1)
-
-    # Gab.load_weights(flags.model_dir + '/Gab.h5')
-    # Gba.load_weights(flags.model_dir + '/Gba.h5')
-    # Da.load_weights(flags.model_dir + '/Da.h5')
-    # Db.load_weights(flags.model_dir + '/Db.h5')
-
-    for epoch in range(0, flags.n_epoch):
-        # reduce lr linearly after 100 epochs, from lr_init to 0
-        if epoch >= 100:
-            new_lr = flags.lr_init - flags.lr_init * (epoch - 100) / 100
-            lr_v.assign(lr_v, new_lr)
-            print("New learning rate %f" % new_lr)
-
-        # train 1 epoch
-        for step, (image_A, image_B) in enumerate(zip(data_A, data_B)):
-            if image_A.shape[0] != flags.batch_size or image_B.shape[0] != flags.batch_size : # if the remaining data in this epoch < batch_size
-                break
-            step_time = time.time()
-            with tf.GradientTape(persistent=True) as tape:
-                # print(image_A.numpy().max())
-                fake_B = Gab(image_A)
-                fake_A = Gba(image_B)
-                cycle_A = Gba(fake_B)
-                cycle_B = Gab(fake_A)
-                logits_fake_B = Db(fake_B)    # TODO: missing image buffer (pool)
-                logits_real_B = Db(image_B)
-                logits_fake_A = Da(fake_A)
-                logits_real_A = Da(image_A)
-                # loss_Da = (tl.cost.mean_squared_error(logits_real_A, tf.ones_like(logits_real_A), is_mean=True) + \  # LSGAN
-                #     tl.cost.mean_squared_error(logits_fake_A, tf.ones_like(logits_fake_A), is_mean=True)) / 2.
-                loss_Da = tf.reduce_mean(tf.math.squared_difference(logits_fake_A, tf.zeros_like(logits_fake_A))) + \
-                    tf.reduce_mean(tf.math.squared_difference(logits_real_A, tf.ones_like(logits_real_A)))
-                # loss_Da = tl.cost.sigmoid_cross_entropy(logits_fake_A, tf.zeros_like(logits_fake_A)) + \
-                    # tl.cost.sigmoid_cross_entropy(logits_real_A, tf.ones_like(logits_real_A))
-                # loss_Db = (tl.cost.mean_squared_error(logits_real_B, tf.ones_like(logits_real_B), is_mean=True) + \ # LSGAN
-                #     tl.cost.mean_squared_error(logits_fake_B, tf.ones_like(logits_fake_B), is_mean=True)) / 2.
-                loss_Db = tf.reduce_mean(tf.math.squared_difference(logits_fake_B, tf.zeros_like(logits_fake_B))) + \
-                    tf.reduce_mean(tf.math.squared_difference(logits_real_B, tf.ones_like(logits_real_B)))
-                # loss_Db = tl.cost.sigmoid_cross_entropy(logits_fake_B, tf.zeros_like(logits_fake_B)) + \
-                #     tl.cost.sigmoid_cross_entropy(logits_real_B, tf.ones_like(logits_real_B))
-                # loss_Gab = tl.cost.mean_squared_error(logits_fake_B, tf.ones_like(logits_fake_B), is_mean=True) # LSGAN
-                loss_Gab = tf.reduce_mean(tf.math.squared_difference(logits_fake_B, tf.ones_like(logits_fake_B)))
-                # loss_Gab = tl.cost.sigmoid_cross_entropy(logits_fake_B, tf.ones_like(logits_fake_B))
-                # loss_Gba = tl.cost.mean_squared_error(logits_fake_A, tf.ones_like(logits_fake_A), is_mean=True) # LSGAN
-                loss_Gba = tf.reduce_mean(tf.math.squared_difference(logits_fake_A, tf.ones_like(logits_fake_A)))
-                # loss_Gba = tl.cost.sigmoid_cross_entropy(logits_fake_A, tf.ones_like(logits_fake_A))
-                # loss_cyc = 10 * (tl.cost.absolute_difference_error(image_A, cycle_A, is_mean=True) + \
-                #     tl.cost.absolute_difference_error(image_B, cycle_B, is_mean=True))
-                loss_cyc = 10. * (tf.reduce_mean(tf.abs(image_A - cycle_A)) + tf.reduce_mean(tf.abs(image_B - cycle_B)))
-                loss_Gab_total = loss_Gab + loss_cyc
-                loss_Gba_total = loss_Gba + loss_cyc
-            grad = tape.gradient(loss_Gab_total, Gab.trainable_weights)
-            optimizer_Gab.apply_gradients(zip(grad, Gab.trainable_weights))
-            grad = tape.gradient(loss_Gba_total, Gba.trainable_weights)
-            optimizer_Gba.apply_gradients(zip(grad, Gba.trainable_weights))
-            grad = tape.gradient(loss_Da, Da.trainable_weights)
-            optimizer_Da.apply_gradients(zip(grad, Da.trainable_weights))
-            grad = tape.gradient(loss_Db, Db.trainable_weights)
-            optimizer_Db.apply_gradients(zip(grad, Db.trainable_weights))
-            # del tape
-            print("Epoch[{}/{}] step[{}/{}] time:{} Gab:{} Gba:{} cyc:{} Da:{} Db:{}".format(\
-                epoch, flags.n_epoch, step, n_step_per_epoch, time.time()-step_time, \
-                loss_Gab, loss_Gba, loss_cyc, loss_Da, loss_Db))
-
-        # visualization
-        outb = Gab(sample_A)
-        outa = Gba(sample_B)
-        tl.vis.save_images(outb.numpy(), [5, 5], flags.sample_dir+'/{}_a2b.png'.format(epoch))
-        tl.vis.save_images(outa.numpy(), [5, 5], flags.sample_dir+'/{}_b2a.png'.format(epoch))
-
-        # save models
-        if epoch % 5:
-            Gab.save_weights(flags.model_dir + '/Gab.h5')
-            Gba.save_weights(flags.model_dir + '/Gba.h5')
-            Da.save_weights(flags.model_dir + '/Da.h5')
-            Db.save_weights(flags.model_dir + '/Db.h5')
-
-def parallel_train(kungfu_option):
-    from kungfu import current_cluster_size, current_rank
-    from kungfu.tensorflow.optimizers import SynchronousSGDOptimizer, SynchronousAveragingOptimizer, PairAveragingOptimizer
-
+def train(parallel, kungfu_option):
     Gab = models.get_G(name='Gab')
     Gba = models.get_G(name='Gba')
     Da  = models.get_D(name='Da')
@@ -133,23 +37,25 @@ def parallel_train(kungfu_option):
     optimizer_Db = tf.optimizers.Adam(lr_v, beta_1=flags.beta_1)
 
     # KungFu: wrap the optimizers
-    if kungfu_option == 'sync-sgd':
-        optimizer_Gab = SynchronousSGDOptimizer(optimizer_Gab)
-        optimizer_Gba = SynchronousSGDOptimizer(optimizer_Gba)
-        optimizer_Da = SynchronousSGDOptimizer(optimizer_Da)
-        optimizer_Db = SynchronousSGDOptimizer(optimizer_Db)
-    elif kungfu_option == 'async-sgd':
-        optimizer_Gab = PairAveragingOptimizer(optimizer_Gab)
-        optimizer_Gba = PairAveragingOptimizer(optimizer_Gba)
-        optimizer_Da = PairAveragingOptimizer(optimizer_Da)
-        optimizer_Db = PairAveragingOptimizer(optimizer_Db)
-    elif kungfu_option == 'sma':
-        optimizer_Gab = SynchronousAveragingOptimizer(optimizer_Gab)
-        optimizer_Gba = SynchronousAveragingOptimizer(optimizer_Gba)
-        optimizer_Da = SynchronousAveragingOptimizer(optimizer_Da)
-        optimizer_Db = SynchronousAveragingOptimizer(optimizer_Db)
-    else:
-        raise RuntimeError('Unknown distributed training optimizer.')
+    if parallel:
+        from kungfu.tensorflow.optimizers import SynchronousSGDOptimizer, SynchronousAveragingOptimizer, PairAveragingOptimizer
+        if kungfu_option == 'sync-sgd':
+            optimizer_Gab = SynchronousSGDOptimizer(optimizer_Gab)
+            optimizer_Gba = SynchronousSGDOptimizer(optimizer_Gba)
+            optimizer_Da = SynchronousSGDOptimizer(optimizer_Da)
+            optimizer_Db = SynchronousSGDOptimizer(optimizer_Db)
+        elif kungfu_option == 'async-sgd':
+            optimizer_Gab = PairAveragingOptimizer(optimizer_Gab)
+            optimizer_Gba = PairAveragingOptimizer(optimizer_Gba)
+            optimizer_Da = PairAveragingOptimizer(optimizer_Da)
+            optimizer_Db = PairAveragingOptimizer(optimizer_Db)
+        elif kungfu_option == 'sma':
+            optimizer_Gab = SynchronousAveragingOptimizer(optimizer_Gab)
+            optimizer_Gba = SynchronousAveragingOptimizer(optimizer_Gba)
+            optimizer_Da = SynchronousAveragingOptimizer(optimizer_Da)
+            optimizer_Db = SynchronousAveragingOptimizer(optimizer_Db)
+        else:
+            raise RuntimeError('Unknown distributed training optimizer.')
 
     # Gab.load_weights(flags.model_dir + '/Gab.h5')
     # Gba.load_weights(flags.model_dir + '/Gba.h5')
@@ -157,12 +63,17 @@ def parallel_train(kungfu_option):
     # Db.load_weights(flags.model_dir + '/Db.h5')
 
     # KungFu: shard the data
-    data_A_shard = []
-    data_B_shard = []
-    for step, (image_A, image_B) in enumerate(zip(data_A, data_B)):
-        if step % current_cluster_size() == current_rank():
-            data_A_shard.append(image_A)
-            data_B_shard.append(image_B)
+    if parallel:
+        from kungfu import current_cluster_size, current_rank
+        data_A_shard = []
+        data_B_shard = []
+        for step, (image_A, image_B) in enumerate(zip(data_A, data_B)):
+            if step % current_cluster_size() == current_rank():
+                data_A_shard.append(image_A)
+                data_B_shard.append(image_B)
+    else:
+        data_A_shard = data_A
+        data_B_shard = data_B
 
     for epoch in range(0, flags.n_epoch):
         # reduce lr linearly after 100 epochs, from lr_init to 0
@@ -172,7 +83,7 @@ def parallel_train(kungfu_option):
             print("New learning rate %f" % new_lr)
 
         # train 1 epoch
-        for step, (image_A, image_B) in enumerate(zip(data_A_shard, data_B_shard)):
+        for step, (image_A, image_B) in enumerate(zip(data_A_shard, data_A_shard)):
             if image_A.shape[0] != flags.batch_size or image_B.shape[0] != flags.batch_size : # if the remaining data in this epoch < batch_size
                 break
             step_time = time.time()
@@ -238,8 +149,13 @@ def parallel_train(kungfu_option):
                 broadcast_variables(optimizer_Da.variables())
                 broadcast_variables(optimizer_Db.variables())
 
-        # KungFu: let the first worker to do visuliazation and checkpoints.
-        if current_rank() == 0:
+        is_chief = not parallel
+        if not is_chief:
+            from kungfu import current_rank
+            is_chief = current_rank() == 0
+
+        # Let the chief worker to do visuliazation and checkpoints.
+        if is_chief:
             # visualization
             outb = Gab(sample_A)
             outa = Gba(sample_B)
@@ -281,9 +197,5 @@ if __name__ == '__main__':
                         help='enable parallel training')
     args = parser.parse_args()
 
-    if args.parallel:
-        parallel_train(args.kf_optimizer)
-    else:
-        train()
-
+    train(args.parallel, args.kf_optimizer)
     eval()
